55e49bb87a7296fb66b6ffe36aa31c85df2d1b5e
Yoann Laala <yoann.laala@gmail.com>
2012-07-06 21:02:15 +0200

    Camera mouvement WIP

--- ARPandaBox/Assets/Scripts/Cube.cs
+++ ARPandaBox/Assets/Scripts/Cube.cs
@@ -6,7 +6,14 @@ public class Cube : MonoBehaviour
 {
 	private Quaternion m_rotationBase;
 	private Quaternion m_rotationRation;
-	private float m_moveRatio = 0.1f;
+	private float m_moveRatio = 1f;
+	private Vector3 m_velocityAcceleration;
+	private Vector3 m_positionAcceleration;
+	private Vector3 m_velocityGyroAcceleration;
+	private Vector3 m_positionGyroAcceleration;
+	private Vector3 m_velocityOld;
+	private float m_threshold = 0.03f;
+	private Rect mAreaRect;
 	
 	void Start () 
 	{
@@ -17,7 +24,7 @@ public class Cube : MonoBehaviour
 		camParent.transform.parent = originalParent; // make the new parent a child of the original parent*/
 		
 		// Is Gyroscope avaible ?
-		if (Input.isGyroAvailable) 
+		if (SystemInfo.supportsGyroscope) 
 		{
 			Input.gyro.enabled = true;
 			
@@ -41,6 +48,15 @@ public class Cube : MonoBehaviour
 				m_rotationRation = new Quaternion(0,0,1,0);
 			}
 		} 
+		
+		Input.location.Start();
+	}
+	
+	private void Reset()
+	{
+		transform.position = Vector3.zero;	
+		m_velocityAcceleration = Vector3.zero;
+		m_positionAcceleration = Vector3.zero;
 	}
 	
 	// Update is called once per frame
@@ -50,21 +66,155 @@ public class Cube : MonoBehaviour
 		///float iphoney=-Input.acceleration.y;
 		//transform.Rotate(Vector3.up*iphoney*5f); 
 		
-		if (Input.isGyroAvailable) 
+		
+		if (SystemInfo.supportsGyroscope) 
 		{
-			//print (Input.gyro.attitude);
+			//if(Input.gyro.userAcceleration.magnitude > 0.03f)
+			//{
+			/*Vector3 filteredAcceleration = Input.gyro.userAcceleration - Input.gyro.gravity;
+			m_velocity += filteredAcceleration * Time.deltaTime;
+			m_position += filteredAcceleration.normalized * Time.deltaTime;		
+			print(m_position);*/
+			//}
+			//Isolating gravity vector
+/*gravity.x = currentAcceleration.x * kFileringFactor + gravity.x * (1.0 - kFileringFactor);
+gravity.y = currentAcceleration.y * kFileringFactor + gravity.y * (1.0 - kFileringFactor);
+gravity.z = currentAcceleration.z * kFileringFactor + gravity.z * (1.0 - kFileringFactor);
+float gravityNorm = sqrt(gravity.x * gravity.x + gravity.y * gravity.y + gravity.z * gravity.z);
+
+//Removing gravity vector from initial acceleration
+filteredAcceleration.x = acceleration.x - gravity.x / gravityNorm;
+filteredAcceleration.y = acceleration.y - gravity.y / gravityNorm;
+filteredAcceleration.z = acceleration.z - gravity.z / gravityNorm;
+
+//Calculating velocity related to time interval
+velocity.x = velocity.x + filteredAcceleration.x * interval;
+velocity.y = velocity.y + filteredAcceleration.y * interval;
+velocity.z = velocity.z + filteredAcceleration.z * interval;
+
+//Finding position
+position.x = position.x + velocity.x * interval * 160;
+position.y = position.y + velocity.y * interval * 230;*/
+			
+			
+			
 			transform.localRotation = Input.gyro.attitude * m_rotationRation;
-			//print(Input.gyro.userAcceleration.magnitude +"="+ Input.gyro.userAcceleration);
-			if(Input.gyro.userAcceleration.magnitude > 0.03f)
+			if(Input.gyro.userAcceleration.magnitude > m_threshold)
 			{
-				print (Input.gyro.userAcceleration.magnitude +"/"+Input.gyro.userAcceleration.normalized * m_moveRatio);
+				m_velocityAcceleration += (Input.gyro.userAcceleration - m_velocityOld) / 2f * m_moveRatio * Time.deltaTime;
+				//m_velocityAcceleration += (Input.acceleration - Input.gyro.gravity) * Time.deltaTime;	
+				//m_positionAcceleration += (Input.acceleration - Input.gyro.gravity).normalized * Time.deltaTime;
+				m_positionAcceleration +=  m_velocityAcceleration * Time.deltaTime;
+				
+				transform.localPosition = m_positionAcceleration;
+				m_velocityOld = Input.gyro.userAcceleration;
+			}
+			//Input.location.
+			//print (Input.gyro.attitude);
+			
+			//print(Input.location.lastData.altitude+"/"+ Input.location.lastData.longitude + "/"+Input.acceleration.normalized+ "/" + Input.gyro.userAcceleration.normalized);
+			/*if(Input.gyro.userAcceleration.magnitude > 0.03f)
+			{
+				LocationInfo info = Input.location.lastData;
+				//print(Input.location.status+"="+info.altitude+"/"+ info.latitude + "/" + info.horizontalAccuracy + "/" + info.verticalAccuracy);
+				print (Input.gyro.userAcceleration);
 				//Input.gyro.
-				Vector3 acc = Input.gyro.userAcceleration.normalized * m_moveRatio;
+				//Vector3 acceleration = Input.gyro.userAcceleration.normalized;
+				Vector3 direction = Input.gyro.userAcceleration * m_moveRatio * Time.deltaTime;
 				//Vector3 acc = Input.gyro.userAcceleration;
 				//acc.z *= -1;
 				//print(Input.gyro.attitude+"/"+Input.gyro.userAcceleration.magnitude +"="+ acc);
-				transform.position += acc;
-			}
+				transform.localPosition += direction;
+			}*/
 		}
+		/*else
+		{
+			transform.position += transform.forward * Time.deltaTime;
+		}*/
 	}
+	
+	void OnGUI()
+	{	
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Acceleration = " +Input.acceleration);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Gyro Acceleration = " +Input.gyro.userAcceleration);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Acceleration Magnitude = " +Input.acceleration.magnitude);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Acceleration SQRMagnitude = " +Input.acceleration.sqrMagnitude);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Gravity = " +Input.gyro.gravity);
+		GUILayout.EndHorizontal();
+		
+		
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Filtered Acceleration = " +(Input.acceleration - Input.gyro.gravity));
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Velocity Acceleration = " +m_velocityAcceleration);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Position Acceleration = " + m_positionAcceleration);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Threshold = " + m_threshold);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Speed = " + m_moveRatio);
+		GUILayout.EndHorizontal();
+		
+		// Setup style for buttons.
+		computePosition();
+        GUIStyle buttonGroupStyle = new GUIStyle(GUI.skin.button);
+        buttonGroupStyle.stretchWidth = true;
+        buttonGroupStyle.stretchHeight = true;
+		GUILayout.BeginArea(mAreaRect);
+        GUILayout.BeginHorizontal(buttonGroupStyle);
+        if (GUILayout.Button("Reset", buttonGroupStyle))
+        {
+      		Reset();
+        }
+		if (GUILayout.Button("- threshold", buttonGroupStyle))
+        {
+      		m_threshold -= 0.01f;
+        }
+		if (GUILayout.Button("+ threshold", buttonGroupStyle))
+        {
+      		m_threshold += 0.01f;
+        }
+		if (GUILayout.Button("- speed", buttonGroupStyle))
+        {
+      		m_moveRatio -= 10f;
+        }
+		if (GUILayout.Button("+ speed", buttonGroupStyle))
+        {
+      		m_moveRatio += 10f;
+        }
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+	}
+	
+	/// Compute the coordinates of the menu depending on the current orientation.
+    private void computePosition()
+    {
+        int areaWidth = Screen.width;
+        int areaHeight = (Screen.height / 5);
+        int areaLeft = 0;
+        int areaTop = Screen.height - areaHeight;
+        mAreaRect = new Rect(areaLeft, areaTop, areaWidth, areaHeight);
+    }
 }
